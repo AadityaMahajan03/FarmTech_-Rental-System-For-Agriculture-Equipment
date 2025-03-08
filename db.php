ee0e59f684b660012eeeeabca2b0102df63be91a
Aaditya Mahajan <mahajanaaditya50@gmail.com>
2025-03-08 16:38:32 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,21 +1,19 @@
 <?php
 
-
-$servername = "localhost";
-$username = "root";
-$password = "aaditya0324";
-$db = "xyz";
-
-
+$servername = getenv("hopper.proxy.rlwy.net");  // Railway MySQL host from environment variable
+$username = getenv("root");  // Railway MySQL username
+$password = getenv("vaGeGgncyJbKewohxvNkQFgNbBIZLmDB");  // Railway MySQL password
+$db = getenv("railway");  // Railway database name
+$port = getenv("54108");  // Railway MySQL port
 
 // Create connection
-$con = mysqli_connect($servername, $username, $password,$db);
-
+$con = mysqli_connect($servername, $username, $password, $db, $port);
 
 // Check connection
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected successfully!";
 }
-
 
 ?>
