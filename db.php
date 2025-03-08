87b151905e8511785037301d4ca5019ebf71cfae
Aaditya Mahajan <mahajanaaditya50@gmail.com>
2025-03-08 16:54:18 +0530

    Update db.php

--- db.php
+++ db.php
@@ -14,7 +14,7 @@ $con = mysqli_connect($servername, $username, $password, $db, $port);
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 } else {
-    echo "Connected successfully!";
+    
 }
 
 ?>
